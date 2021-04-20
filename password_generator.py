cb4d910563201362dc3e1bf0edac2bfa8d4e8ed4
mzebin <mohammed.zebin7@gmail.com>
2021-04-20 14:30:26 +0400

    Working on Password Generator

---
+++ password_generator.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+UPPERCASE = string.ascii_uppercase
+LOWERCASE = string.ascii_lowercase
+DIGITS = string.digits
+PUNCTUATION = string.punctuation.replace("-", "")
+
+
+def generator(**kwargs):
+    pass
+
+
+def get_characters(**kwargs):
+    pass
