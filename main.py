0c7f8f8c8615083714a4cc3fdbb7c9212278a0ec
mzebin <mohammed.zebin7@gmail.com>
2021-04-20 15:50:06 +0400

    Home Page Completed

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/generate/", methods=["GET", "POST"])
+def generate():
+    return "Generate"
+
+
+@app.route("/copy/")
+def copy():
+    return "Copy"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
