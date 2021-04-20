5715b219fa21e574f74668ee69a2e7be90cb54a2
mzebin <mohammed.zebin7@gmail.com>
2021-04-20 14:39:38 +0400

    Working on Password Generator

--- password_generator.py
+++ password_generator.py
@@ -8,7 +8,17 @@ PUNCTUATION = string.punctuation.replace("-", "")
 
 
 def generator(**kwargs):
-    pass
+    length = kwargs.get("length", 14)
+    characters = get_characters(**kwargs)
+
+    password = ""
+    for i in range(length):
+        if i % 3 == 0 and i != 0 and i + 1 != length:
+            password += "-"
+        else:
+            password += random.choice(characters)
+
+    return password
 
 
 def get_characters(**kwargs):
